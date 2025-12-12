513c83817cef603f669e1cd33f86ef1274db4c3e
Floydous <218109625+floydous@users.noreply.github.com>
2025-12-12 14:00:25 +0800

    mobile navbar panel

--- public/js/index.js
+++ public/js/index.js
@@ -1,19 +1,34 @@
 const Sections = document.querySelectorAll("section[id]");
 const NavIndex = document.querySelector(".nav-index");
 const NavText = document.querySelector(".nav-text");
+const MenuPanel = document.getElementById("menu");
+const MenuButton = document.getElementById("menu-btn");
+const MobileLinks = MenuPanel.querySelectorAll("a");
+let currentSection = null;
 const SectionObserver = new IntersectionObserver((entries) => {
     const process = (entry) => {
         if (entry.isIntersecting) {
             const SectionID = entry.target.getAttribute("id");
             const IntersectingElement = entry.target;
             NavIndex.textContent = "0" + (Array.from(Sections).indexOf(IntersectingElement) + 1).toString();
+            currentSection = IntersectingElement;
             NavText.textContent = SectionID;
         }
     };
     entries.forEach((entry) => process(entry));
 }, { threshold: 0.5 });
-Sections.forEach((section) => {
-    SectionObserver.observe(section);
-});
+Sections.forEach((section) => SectionObserver.observe(section));
+function triggerSidebar() {
+    MenuPanel.classList.toggle("translate-x-full");
+    MenuButton.textContent = MenuPanel.classList.contains("translate-x-full") ? "[Menu]" : "[Close]";
+    document.body.style.overflow = MenuPanel.classList.contains("translate-x-full") ? "auto" : "hidden";
+    if (currentSection) {
+        const nthSection = Array.from(Sections).indexOf(currentSection);
+        MobileLinks.forEach((element) => element.classList.remove("text-neon"));
+        MobileLinks[nthSection].classList.add("text-neon");
+    }
+}
+MobileLinks.forEach((element) => element.addEventListener("click", triggerSidebar));
+MenuButton.addEventListener("click", triggerSidebar);
 export {};
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,12 @@ const Sections = document.querySelectorAll("section[id]") as NodeListOf<HTMLElem
 const NavIndex = document.querySelector(".nav-index") as HTMLElement;
 const NavText = document.querySelector(".nav-text") as HTMLElement;
 
+const MenuPanel = document.getElementById("menu") as HTMLElement;
+const MenuButton = document.getElementById("menu-btn") as HTMLElement;
+const MobileLinks = MenuPanel.querySelectorAll("a") as NodeListOf<HTMLElement>;
+
+let currentSection: HTMLElement | null = null;
+
 const SectionObserver = new IntersectionObserver(
 	(entries) => {
 		const process = (entry: IntersectionObserverEntry) => {
@@ -10,6 +16,7 @@ const SectionObserver = new IntersectionObserver(
 				const IntersectingElement = entry.target as HTMLElement;
 
 				NavIndex.textContent = "0" + (Array.from(Sections).indexOf(IntersectingElement) + 1).toString();
+				currentSection = IntersectingElement;
 				NavText.textContent = SectionID;
 			}
 		};
@@ -19,6 +26,20 @@ const SectionObserver = new IntersectionObserver(
 	{ threshold: 0.5 }
 );
 
-Sections.forEach((section) => {
-	SectionObserver.observe(section);
-});
+Sections.forEach((section) => SectionObserver.observe(section));
+
+function triggerSidebar() {
+	MenuPanel.classList.toggle("translate-x-full");
+	MenuButton.textContent = MenuPanel.classList.contains("translate-x-full") ? "[Menu]" : "[Close]";
+	document.body.style.overflow = MenuPanel.classList.contains("translate-x-full") ? "auto" : "hidden";
+
+	if (currentSection) {
+		const nthSection = Array.from(Sections).indexOf(currentSection);
+
+		MobileLinks.forEach((element) => element.classList.remove("text-neon"));
+		MobileLinks[nthSection]!.classList.add("text-neon");
+	}
+}
+
+MobileLinks.forEach((element) => element.addEventListener("click", triggerSidebar));
+MenuButton.addEventListener("click", triggerSidebar);
